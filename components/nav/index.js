dfa90f7cea272fbc5ad0963ccaf543801adadda4
maccali <guilherme.maccali@universo.univates.br>
2020-04-11 00:06:25 -0300

    First Commit

---
+++ components/nav/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import Link from 'next/link'
+import Head from 'next/head'
+import styles from './nav.module.css'
+
+const Nav = () => (
+  <div className="container-fluid" className={styles.container}>
+    <div className="container">
+
+      <nav className={styles.nav}>
+        <Head>
+          <meta charset='utf-8' />
+          <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+          <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
+          <link rel='manifest' href='/manifest.json' />
+        </Head>
+        <div className={styles.img}>
+          <img src="/icons/icon48.png" />
+        </div>
+        <ul className={styles.menu}>  
+          <Link href="/shows">
+            Shows
+          </Link>
+          <Link href="/artistas">
+            Artistas
+          </Link>
+        </ul>
+
+        <style>
+
+        </style>
+      </nav>
+    </div>
+  </div>
+)
+
+export default Nav
---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '../public/libs/bootstrap/bootstrap.min.css'
+import '../public/libs/fontawesome/css/all.min.css'
+import '../public/custom/css/styles.css'
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
