3064150c6bb3ec2f54517e6a680318098f156908
maccali <guilherme.maccali@universo.univates.br>
2020-04-19 16:16:56 -0300

    Portal, Ajusts

---
+++ components/cards/tool/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Link from 'next/link'
+import styles from './tool.module.css'
+import Parser from 'html-react-parser';
+
+
+const Tool = (props) => (
+    <>
+        <Link href={props.tool.link}>
+            <a className={styles.card}>
+                <div className={styles.icon}>
+                    {Parser(props.tool.icon)}
+                </div>
+                <div className={styles.name}>
+                    {props.tool.name}
+                </div>
+                <div className={styles.description}>
+                    {props.tool.description}
+                </div>
+            </a>
+        </ Link>
+    </>
+)
+
+export default Tool
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,23 @@
 import Head from 'next/head'
 import Nav from '../components/nav'
-import Search from '../components/search'
+import Footer from '../components/footer'
+import CardTool from '../components/cards/tool'
+
+const toolList = [
+  {
+    name: "QRCode Gen",
+    icon: "<i className='fas fa-qrcode'></i>",
+    description: "Generate QRCode from any Link or Text",
+    link: "/qrcode"
+  },
+  {
+    name: "Short Link",
+    icon: "<i className='fas fa-link'></i>",
+    description: "Encurte o link e escolha qual link você quer usar",
+    link: "/short-link"
+  }
+]
+
 
 const Home = () => (
   <>
@@ -8,18 +25,25 @@ const Home = () => (
     <Head>
       <title>Azeit Tools</title>
     </Head>
-
     <main>
-      <div className="container-fluid bg-primary">
+      <div className="container-fluid bg-primary container-full-height">
         <div className="container">
           <div className="row">
             <div className="col-12">
-              <Search />
+              <h1>Our tools</h1>
             </div>
+          </div>
+          <div className="row">
+            {toolList.map(tool => (
+              <div className="col-12 col-sm-3 col-md-4">
+                <CardTool tool={tool} />
+              </div>
+            ))}
           </div>
         </div>
       </div>
     </main>
+    <Footer />
   </>
 )
 
