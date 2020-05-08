088657fa24b63e69380d775ef98ffdfea19e1153
maccali <guilherme.maccali@universo.univates.br>
2020-05-08 17:08:04 -0300

    Ajust tags SEO 2

--- pages/day/[day].js
+++ pages/day/[day].js
@@ -73,7 +73,7 @@ class Day extends React.Component {
           <meta name="og:description" property="og:description" content={descriptionCards} />
           <meta name="og:url" property="og:url" content={link} />
 
-          <meta name={`og:${media_type}`} property={`og:${media_type}`} content={url} />
+          <meta name="og:image" property="og:image" content={url} />
           <meta name="og:type" property="og:type" content={media_type} />
 
           {/* Twitter Tags */}
