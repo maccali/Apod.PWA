8d928fd52070532ee599452d43ccea5bc668f523
maccali <guilherme.maccali@universo.univates.br>
2020-05-10 23:55:21 -0300

    Menu formed

--- components/utils/nav/index.js
+++ components/utils/nav/index.js
@@ -15,9 +15,42 @@ const Nav = () => (
           <li>
             <div className={styles.img}>
               <Link href="/">
-                <img src="/icons/icon48.png" alt="Site Logo" />
+                {/* <a className={styles.seta}> */}
+                {/* TUDO: gerar imagem com transparência */}
+                  <img src="/icons/icon48.png" alt="Site Logo" />
+                {/* </a> */}
               </Link>
             </div>
+          </li>
+        </ul>
+        <ul className={styles.menu}>
+          <li>
+            <Link href="/favorites">
+              <a className={styles.seta}>
+                <i className="far fa-heart"></i>
+              </a>
+            </Link>
+          </li>
+          <li>
+            {/* <Link href="/images/1"> */}
+            <a className={styles.seta}>
+              <i className="far fa-calendar-alt"></i>
+            </a>
+            {/* </Link> */}
+          </li>
+          <li>
+            <Link href="/images/1">
+              <a className={styles.seta}>
+                <i className="fas fa-list-ul"></i>
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a className={styles.seta}>
+                <i className="fas fa-info-circle"></i>
+              </a>
+            </Link>
           </li>
         </ul>
       </nav>
