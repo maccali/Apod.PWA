acbc83a011f2acef9fffd06dce9b930f1d243aef
maccali <guilherme.maccali@universo.univates.br>
2020-05-12 17:33:11 -0300

    Add env files

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const withPWA = require('next-pwa')
+const Dotenv = require("dotenv-webpack");
 
 module.exports = withPWA({
   pwa: {
@@ -6,5 +7,11 @@ module.exports = withPWA({
     importScripts: [
       '/worker.js'
     ]
+  },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Add the new plugin to the existing webpack plugins
+    config.plugins.push(new Dotenv({ silent: true }));
+
+    return config;
   }
 })
