35c5c6f04bcc2d5f8e6ffe33f444c2cd4b87fde1
maccali <guilherme.maccali@universo.univates.br>
2020-05-19 01:59:39 -0300

    Menu end Footer Ajusts

--- components/utils/footer/index.js
+++ components/utils/footer/index.js
@@ -2,15 +2,17 @@ import React from 'react'
 import styles from './footer.module.css'
 
 const Footer = () => (
-  <div className="container-fluid" className={styles.container}>
-    <div className="container">
-      <footer>
-        <p>
-         Explore <i className="fab fa-wpexplorer"></i>
-        </p>
-      </footer>
-    </div>
-  </div>
+  <>
+    {/* <div className="container-fluid" className={styles.container}>
+      <div className="container">
+        <footer>
+          <p>
+            Explore <i className="fab fa-wpexplorer"></i>
+          </p>
+        </footer>
+      </div>
+    </div> */}
+  </>
 )
 
 export default Footer
--- components/utils/nav/index.js
+++ components/utils/nav/index.js
@@ -8,63 +8,65 @@ function Nav() {
   const [menuActive, setMenuActive] = useState(0);
 
   return (<>
-    <div className="container-fluid" className={styles.container}>
+    <div className="container-fluid" className={styles.cont}>
+      <div className="container" >
 
-      <nav className={styles.nav}>
-        <ul className={styles.menu}>
-          <li>
-            <a onClick={() => Router.back()} className={styles.seta}><i className="fas fa-arrow-left"></i></a>
-          </li>
-          <li>
-            <div className={styles.img}>
-              <Link href="/">
-                {/* <a className={styles.seta}> */}
-                {/* TUDO: gerar imagem com transparência */}
-                <img src="/icons/icon48.png" alt="Site Logo" />
-                {/* </a> */}
+        <nav className={styles.nav}>
+          <ul className={styles.menu}>
+            <li>
+              <a onClick={() => Router.back()} className={styles.seta}><i className="fas fa-arrow-left"></i></a>
+            </li>
+            <li>
+              <div className={styles.img}>
+                <Link href="/">
+                  {/* <a className={styles.seta}> */}
+                  {/* TUDO: gerar imagem com transparência */}
+                  <img src="/icons/icon48.png" alt="Site Logo" />
+                  {/* </a> */}
+                </Link>
+              </div>
+            </li>
+          </ul>
+          <ul className={styles.menu}>
+            <li>
+              <Link href="/images/1">
+                <a className={styles.seta}>
+                  <i className="fas fa-list-ul"></i>
+                </a>
+              </Link>
+            </li>
+            {/* <ul className={styles.menu}>
+              <li>
+                <a onClick={() => setMenuActive(!menuActive)} className={styles.seta}><i className="fas fa-ellipsis-v"></i></a>
+              </li>
+            </ul> */}
+          </ul>
+        </nav>
+        <div className={(menuActive) ? `${styles.contaside} ${styles.contasideativado}` : styles.contaside}>
+          <div className={styles.contasidefix}>
+            <div className={styles.headercont}>
+              <p>Menu</p>
+              <a onClick={() => setMenuActive(!menuActive)}>
+                <i className="fas fa-times"></i>
+              </a>
+            </div>
+            <div className={styles.cardmenu}>
+              <div>
+                <img src="/icons/icon96.png" alt="Foto do Perfil" />
+              </div>
+              <span>Apod Space</span>
+            </div>
+            <div className={styles.menulist}>
+              <Link href="/calendar">
+                <a className={styles.menuitem}><span><i className="far fa-calendar-alt"></i></span><p>Calendar</p></a>
+              </Link>
+              <Link href="/favorites">
+                <a className={styles.menuitem}><span><i className="far fa-heart"></i></span><p>Favorites</p></a>
+              </Link>
+              <Link href="/about">
+                <a className={styles.menuitem}><span><i className="fas fa-info-circle"></i></span><p>About</p></a>
               </Link>
             </div>
-          </li>
-        </ul>
-        <ul className={styles.menu}>
-          <li>
-            <Link href="/images/1">
-              <a className={styles.seta}>
-                <i className="fas fa-list-ul"></i>
-              </a>
-            </Link>
-          </li>
-          {/* <ul className={styles.menu}>
-            <li>
-              <a onClick={() => setMenuActive(!menuActive)} className={styles.seta}><i className="fas fa-ellipsis-v"></i></a>
-            </li>
-          </ul> */}
-        </ul>
-      </nav>
-      <div className={(menuActive) ? `${styles.contaside} ${styles.contasideativado}` : styles.contaside}>
-        <div className={styles.contasidefix}>
-          <div className={styles.headercont}>
-            <p>Menu</p>
-            <a onClick={() => setMenuActive(!menuActive)}>
-              <i className="fas fa-times"></i>
-            </a>
-          </div>
-          <div className={styles.cardmenu}>
-            <div>
-              <img src="/icons/icon96.png" alt="Foto do Perfil" />
-            </div>
-            <span>Apod Space</span>
-          </div>
-          <div className={styles.menulist}>
-            <Link href="/calendar">
-              <a className={styles.menuitem}><span><i className="far fa-calendar-alt"></i></span><p>Calendar</p></a>
-            </Link>
-            <Link href="/favorites">
-              <a className={styles.menuitem}><span><i className="far fa-heart"></i></span><p>Favorites</p></a>
-            </Link>
-            <Link href="/about">
-              <a className={styles.menuitem}><span><i className="fas fa-info-circle"></i></span><p>About</p></a>
-            </Link>
           </div>
         </div>
       </div>
