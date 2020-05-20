4044d6294b164dd77084b4d71f6e8186f6688ad9
maccali <guilherme.maccali@universo.univates.br>
2020-05-20 14:01:09 -0300

    Add Util for calendar

---
+++ helpers/util.js
@@ -0,0 +1,13 @@
+const UtilHelper = {
+
+  rangeInt: (start, end) => {
+    var arrNumbers = []
+    for (var i = start; i < end; i++) {
+      arrNumbers.push(i)
+    }
+    return arrNumbers;
+  }
+
+}
+
+export default UtilHelper
