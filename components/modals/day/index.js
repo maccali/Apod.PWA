d407f3a51ef23099b132d859144b84af9b99b485
maccali <guilherme.maccali@universo.univates.br>
2020-06-07 02:35:30 -0300

    ajust on modal

--- components/modals/day/index.js
+++ components/modals/day/index.js
@@ -8,17 +8,20 @@ function ModalDay({ day, open, closeModal }) {
   return (
     <>
       <div className={`${styles.cont} ${(open) ? styles.open : ''}`}>
-        {/* <div className="container-fluid"> */}
-        <div className={styles.nav}>
-          <div className="container">
-            <div className={styles.item}>
-              <a onClick={() => closeModal()}><i className="fas fa-times"></i></a>
+
+        <div className={styles.scroll}>
+          <div className="container-fluid">
+            <div className={styles.nav}>
+              <div className="container ped-lr-0">
+                <div className={styles.item}>
+                  <a onClick={() => closeModal()}><i className="fas fa-times"></i></a>
+                </div>
+              </div>
             </div>
           </div>
-          {/* </div> */}
-        </div>
-        <div className={styles.scroll}>
+          {/* <div className={styles.preday}></div> */}
           <DayContent day={day} />
+          {/* <div className={styles.preday}></div> */}
         </div>
       </div>
     </>
