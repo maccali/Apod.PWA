d4d335de681fa2fd2599fed49db5ea2147c17d58
maccali <guilherme.maccali@universo.univates.br>
2020-06-08 16:32:49 -0300

    Modal Test

--- components/modals/day/index.js
+++ components/modals/day/index.js
@@ -9,26 +9,22 @@ function ModalDay({ day, open, closeModal }) {
     <>
       <div className={(open) ? `${styles.contaside} ${styles.contasideativado}` : styles.contaside}>
         <div className={styles.contasidefix}>
-          <div className="container-fluid ped-lr-0">
-            <div className="container">
-              <div className="row">
-                <div className="col-12">
-                  <div className={styles.headercont}>
-                    <div className={styles.img}>
-                      <img src="/icons/icon126t.png" alt="Site Logo" />
-                    </div>
-                    <a onClick={() => closeModal(!open)} aria-label="Close Menu">
-                      <i className="fas fa-times"></i>
-                    </a>
+          <div className="container">
+            <div className="row">
+              <div className="col-12">
+                <div className={styles.headercont}>
+                  <div className={styles.img}>
+                    <img src="/icons/icon126t.png" alt="Site Logo" />
                   </div>
-                  <div className="col-12">
-                    <div className={styles.spacer}>
-                    </div>
-                    <DayContent day={day} />
-                  </div>
+                  <a onClick={() => closeModal(!open)} aria-label="Close Menu">
+                    <i className="fas fa-times"></i>
+                  </a>
                 </div>
               </div>
             </div>
+          </div>
+          <div className="col-12">
+            <DayContent day={day} />
           </div>
         </div>
       </div>
