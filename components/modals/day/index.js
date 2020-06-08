f131f86b7ec2a50a2c103c60899866d7c8919948
maccali <guilherme.maccali@universo.univates.br>
2020-06-08 16:53:40 -0300

    Modal Test

--- components/modals/day/index.js
+++ components/modals/day/index.js
@@ -9,22 +9,26 @@ function ModalDay({ day, open, closeModal }) {
     <>
       <div className={(open) ? `${styles.contaside} ${styles.contasideativado}` : styles.contaside}>
         <div className={styles.contasidefix}>
-          <div className="container">
-            <div className="row">
-              <div className="col-12">
-                <div className={styles.headercont}>
-                  <div className={styles.img}>
-                    <img src="/icons/icon126t.png" alt="Site Logo" />
+          <div className={`container-fluid ${styles.fixedcont}`}>
+            <div className="container">
+              <div className="row">
+                <div className="col-12">
+                  <div className={styles.headercont}>
+                    <div className={styles.img}>
+                      <img src="/icons/icon126t.png" alt="Site Logo" />
+                    </div>
+                    <a onClick={() => closeModal(!open)} aria-label="Close Menu">
+                      <i className="fas fa-times"></i>
+                    </a>
                   </div>
-                  <a onClick={() => closeModal(!open)} aria-label="Close Menu">
-                    <i className="fas fa-times"></i>
-                  </a>
                 </div>
               </div>
             </div>
           </div>
           <div className="col-12">
+            <div className={styles.spacer}></div>
             <DayContent day={day} />
+            <div className={styles.spacer}></div>
           </div>
         </div>
       </div>
