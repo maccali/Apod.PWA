8d1e8bc1835a7bf4264019e5c546b5a864fbb2a5
maccali <guilherme.maccali@universo.univates.br>
2020-07-04 16:36:44 -0300

    Add, copy link in day content

--- components/cards/social/index.js
+++ components/cards/social/index.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './social.module.css'
 
 import { AiOutlineShareAlt } from "react-icons/ai";
 import { FaFacebook, FaTwitter } from "react-icons/fa";
+import { FiLink, FiCheckCircle } from "react-icons/fi";
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 import ShareLinks from '../../../models/shareLinks'
 
 function Social({ title, text, url }) {
+
+  const [copied, setCopied] = useState(false);
 
   function share() {
     console.log(title, text, url)
@@ -15,23 +19,45 @@ function Social({ title, text, url }) {
     })
   }
 
+  async function chackmate() {
+    setCopied(true)
+    await sleep(3000);
+    setCopied(false)
+  }
+
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
   return (
     <>
-      {(navigator.share === undefined) ?
-        <div className={styles.card}>
-          <a target="_blank" rel="noopener noreferrer" href={`${ShareLinks.facebook}${url}`}>
-            <FaFacebook />
+      <div className={styles.card}>
+        <CopyToClipboard
+          text={url}
+          onCopy={() => chackmate()}
+        >
+          <a className={styles.iconchange}>
+            {copied ?
+              <FiCheckCircle /> :
+              <FiLink />}
           </a>
-          <a target="_blank" rel="noopener noreferrer" href={`${ShareLinks.twitter}${url}`}>
-            <FaTwitter />
-          </a>
-        </div>
-        : <div className={styles.card}>
-          <a onClick={() => share()}>
-            <AiOutlineShareAlt />
-          </a>
-        </div>
-      }
+        </CopyToClipboard>
+        {(navigator.share === undefined) ?
+          <>
+            <a target="_blank" rel="noopener noreferrer" href={`${ShareLinks.facebook}${url}`}>
+              <FaFacebook />
+            </a>
+            <a target="_blank" rel="noopener noreferrer" href={`${ShareLinks.twitter}${url}`}>
+              <FaTwitter />
+            </a>
+          </>
+          : <>
+            <a onClick={() => share()}>
+              <AiOutlineShareAlt />
+            </a>
+          </>
+        }
+      </div>
     </>
   )
 }
--- components/content/day/index.js
+++ components/content/day/index.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-// import { motion } from "framer-motion"
 import styles from './day.module.css'
 
 import Social from '../../cards/social'
