430862ef3211944193313b95cddbf37bc678349b
maccali <guilherme.maccali@universo.univates.br>
2020-07-16 23:39:27 -0300

    Remove loaders

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,7 @@ import '../public/custom/css/btns.css'
 import 'react-datepicker/dist/react-datepicker.css';
 import 'react-lazy-load-image-component/src/effects/opacity.css';
 
-import NextNprogress from '../components/utils/loader';
+// import NextNprogress from '../components/utils/loader';
 import Meta from '../components/utils/Meta';
 import Offline from '../components/utils/offline'
 
@@ -18,14 +18,14 @@ import Offline from '../components/utils/offline'
 export default function MyApp({ Component, pageProps }) {
   return <>
     {/* Other Custom Logic */}
-    <NextNprogress
+    {/* <NextNprogress
       color="#fff"
       colorSecondary="#2c2c7b"
       startPosition={0.3}
       stopDelayMs={200}
       height={5}
       options={{ easing: 'ease', speed: 500 }}
-    />
+    /> */}
     <Offline />
     <Meta />
     <Component {...pageProps} />
