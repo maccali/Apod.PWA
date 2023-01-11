85efcd2a2db2eccab4f29313a23f98298655a93c
maccali <guilherme.maccali@universo.univates.br>
2023-01-11 19:47:15 -0300

    fix: add anoptimazed to avoid VERCEL limits

--- next.config.js
+++ next.config.js
@@ -1,49 +1,50 @@
 /** @type {import('next').NextConfig} */
 
-const withPWA = require('next-pwa')
-const Dotenv = require('dotenv-webpack')
-const runtimeCaching = require('next-pwa/cache')
-const util = require('util')
+const withPWA = require("next-pwa");
+const Dotenv = require("dotenv-webpack");
+const runtimeCaching = require("next-pwa/cache");
+const util = require("util");
 
-const cacheTime = 365 * 24 * 60 * 60 // 365 days
+const cacheTime = 365 * 24 * 60 * 60; // 365 days
 
-runtimeCaching.map(item => {
-  item.options.expiration.maxAgeSeconds = cacheTime
-  if (item.options.cacheName === 'others') {
-    item.options.expiration.maxEntries = 200
+runtimeCaching.map((item) => {
+  item.options.expiration.maxAgeSeconds = cacheTime;
+  if (item.options.cacheName === "others") {
+    item.options.expiration.maxEntries = 200;
   }
-  if (item.options.cacheName === 'static-image-assets') {
-    item.options.expiration.maxEntries = 100
+  if (item.options.cacheName === "static-image-assets") {
+    item.options.expiration.maxEntries = 100;
   }
-})
+});
 
 // console.log('😜 RUNTIME CACHE =>', JSON.stringify(runtimeCaching, null, 4))
 // console.log('😜 RUNTIME CACHE =>', runtimeCaching)
 
 module.exports = withPWA({
   pwa: {
-    dest: 'public',
-    importScripts: ['/worker.js'],
-    runtimeCaching
+    dest: "public",
+    importScripts: ["/worker.js"],
+    runtimeCaching,
   },
-  webpack: config => {
+  webpack: (config) => {
     // Add the new plugin to the existing webpack plugins
-    config.plugins.push(new Dotenv({ silent: true }))
+    config.plugins.push(new Dotenv({ silent: true }));
 
-    return config
+    return config;
   },
   env: {
-    NASA_API_KEY: process.env.NASA_API_KEY
+    NASA_API_KEY: process.env.NASA_API_KEY,
   },
   images: {
+    unoptimized: true,
     deviceSizes: [320, 640, 768, 1024, 1600],
-    domains: ['apod.nasa.gov']
+    domains: ["apod.nasa.gov"],
   },
   typescript: {
-    ignoreBuildErrors: true
+    ignoreBuildErrors: true,
   },
   eslint: {
-    ignoreDuringBuilds: true
+    ignoreDuringBuilds: true,
   },
-  runtime: 'experimental-edge'
-})
+  runtime: "experimental-edge",
+});
