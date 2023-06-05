1eb1218a6bffdb0081c30d68b035135676de4b19
maccali <guilherme.maccali@universo.univates.br>
2023-06-04 21:47:12 -0300

    fix: chack vars

--- src/helpers/date.ts
+++ src/helpers/date.ts
@@ -1,88 +1,89 @@
-import api from '../services/api'
+import api from "../services/api";
 
 const DateHelper = {
   validDate: (date: string) => {
-    const matches = /(\d{4})[-](\d{2})[-](\d{2})/.exec(date)
+    const matches = /(\d{4})[-](\d{2})[-](\d{2})/.exec(date);
     if (matches == null) {
-      return false
+      return false;
     }
-    const dia = Number(matches[3])
-    const mes = Number(matches[2]) - 1
-    const ano = Number(matches[1])
-    const data = new Date(ano, mes, dia)
+    const dia = Number(matches[3]);
+    const mes = Number(matches[2]) - 1;
+    const ano = Number(matches[1]);
+    const data = new Date(ano, mes, dia);
     return (
       data.getDate() == dia &&
       data.getMonth() == mes &&
       data.getFullYear() == ano
-    )
+    );
   },
 
   validDateAndPast: (date: string) => {
-    const tDate = date
+    const tDate = date;
     if (DateHelper.validDate(date)) {
-      const arrDate = tDate.split('-')
+      const arrDate = tDate.split("-");
       const date = new Date(
         Number(arrDate[0]),
         Number(arrDate[1]) - 1,
         Number(arrDate[2])
-      )
-      const today = new Date()
-      return date < today
+      );
+      const today = new Date();
+      return date < today;
     } else {
-      return false
+      return false;
     }
   },
 
   dateToNasaFormat: (date: string) => {
-    const nasaDate = new Date(date).toISOString().split('T')[0]
-    return nasaDate
+    const nasaDate = new Date(date).toISOString().split("T")[0];
+    return nasaDate;
   },
 
   todayNasaFormat: () => {
-    const date = new Date()
-    const nasaDate = DateHelper.dateToNasaFormat(String(date))
-    return nasaDate
+    const date = new Date();
+    const nasaDate = DateHelper.dateToNasaFormat(String(date));
+    return nasaDate;
   },
 
   nasaFormatMinusOne: (nasaDate: string) => {
-    const arrDate = nasaDate.split('-')
+    const arrDate = nasaDate.split("-");
     const date = new Date(
       Number(arrDate[0]),
       Number(arrDate[1]) - 1,
       Number(arrDate[2])
-    )
-    date.setDate(date.getDate() - 1)
-    const dateIso = date.toISOString().split('T')[0]
-    return dateIso
+    );
+    date.setDate(date.getDate() - 1);
+    const dateIso = date.toISOString().split("T")[0];
+    return dateIso;
   },
 
   nasaFormatPlusOne: (nasaDate: string) => {
-    const arrDate = nasaDate.split('-')
+    const arrDate = nasaDate.split("-");
     const date = new Date(
       Number(arrDate[0]),
       Number(arrDate[1]) - 1,
       Number(arrDate[2])
-    )
-    date.setDate(date.getDate() + 1)
-    const dateIso = date.toISOString().split('T')[0]
-    return dateIso
+    );
+    date.setDate(date.getDate() + 1);
+    const dateIso = date.toISOString().split("T")[0];
+    return dateIso;
   },
 
   validDayFilter: (arrUrls: Array<DayCustomFace>) => {
-    const arrOfDays = arrUrls.filter(theDate => {
+    const arrOfDays = arrUrls.filter((theDate) => {
       if (theDate.day != undefined) {
-        return theDate.day
+        return theDate.day;
       }
-    })
+    });
 
-    return arrOfDays
+    return arrOfDays;
   },
 
   daysCombine: async (startDate: string, numberOfDays: number) => {
-    const key = process.env.NASA_API_KEY
-    let nowDate = startDate
-    let url = `/apod?api_key=${key}&date=${nowDate}`
-    const arrDayUrls: Array<DayCustomFace> = []
+    const key = process.env.NASA_API_KEY;
+    console.log("process.env.NASA_API_KEY", process.env.NASA_API_KEY);
+    let nowDate = startDate;
+    let url = `/apod?api_key=${key}&date=${nowDate}`;
+    const arrDayUrls: Array<DayCustomFace> = [];
 
     for (let i = 0; i < numberOfDays; i++) {
       await api
@@ -90,40 +91,40 @@ const DateHelper = {
         .then((response: any) => {
           if (response.status === 200) {
             const day: DayFace = {
-              copyright: response.data['copyright'],
-              date: response.data['date'],
-              explanation: response.data['explanation'],
-              mediaType: response.data['media_type'],
-              serviceVersion: response.data['service_version'],
-              title: response.data['title'],
-              url: response.data['url'],
-              hdUrl: response.data['hdurl']
-            }
+              copyright: response.data["copyright"],
+              date: response.data["date"],
+              explanation: response.data["explanation"],
+              mediaType: response.data["media_type"],
+              serviceVersion: response.data["service_version"],
+              title: response.data["title"],
+              url: response.data["url"],
+              hdUrl: response.data["hdurl"],
+            };
             arrDayUrls.push({
               order: i,
               url,
-              day
-            })
+              day,
+            });
           }
-          return
+          return;
         })
         .catch((_err: any) => {
           arrDayUrls.push({
             order: i,
             url,
-            error: true
-          })
-          return
-        })
+            error: true,
+          });
+          return;
+        });
 
-      nowDate = DateHelper.nasaFormatMinusOne(nowDate)
-      url = `/apod?api_key=${key}&date=${nowDate}`
+      nowDate = DateHelper.nasaFormatMinusOne(nowDate);
+      url = `/apod?api_key=${key}&date=${nowDate}`;
     }
 
-    const arrOfValidDays = DateHelper.validDayFilter(arrDayUrls)
+    const arrOfValidDays = DateHelper.validDayFilter(arrDayUrls);
 
-    return arrOfValidDays
-  }
-}
+    return arrOfValidDays;
+  },
+};
 
-export default DateHelper
+export default DateHelper;
